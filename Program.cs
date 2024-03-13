624eab9ec34cf129067a7998ceaa34f35df3d7eb
Manuele Santoro <diabolele@gmail.com>
2024-03-13 18:16:32 +0000

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.Design;
+
+namespace Repairing_the_Clock_Tower
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Write a number");
+            int num = Convert.ToInt32(Console.ReadLine());
+            if (num % 2 == 0) 
+            Console.WriteLine("Tick");
+           else
+
+                Console.WriteLine("Tock");
+
+
+        }
+    }
+}
